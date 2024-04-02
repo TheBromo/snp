6da63be3e816729f6f3868cb1fb32b94b7280385
Manuel <manuel@strenge.ch>
2024-04-02 20:39:40 +0000

    added clear method

--- P06_Personen_Verwaltung_Linked_List/work/personen-verwaltung/src/list.c
+++ P06_Personen_Verwaltung_Linked_List/work/personen-verwaltung/src/list.c
@@ -4,7 +4,9 @@
 
 #include "list.h"
 #include <stdlib.h>
-const linked_list_t linked_list;
+
+node_t headElement;
+node_t * const anchor = &headElement;
 
 // Remove: der Benutzer wird aufgefordert, die Daten einer zu löschenden Person einzugeben
 void remove_p(person_t person)
@@ -13,21 +15,23 @@ void remove_p(person_t person)
 
 // Clear: alle Personen werden gelöscht
 void clear_p()
-{
+{ 
+   node_t *current = anchor->next; 
+   while (current != anchor)
+   {
+        node_t *temp = current;
+        current = current->next;
+        free(temp);
+   }
+   anchor->next = anchor;
 }
 
 // Insert: der Benutzer wird aufgefordert, eine Person einzugeben
-void insert_p(person_t person){
+void insert_p(person_t person)
+{
 }
 
 node_t *nalloc()
 {
     return (node_t *)malloc(sizeof(node_t));
-}
-
-void create_linked_list()
-{
-    node_t *anchor = nalloc();
-    linked_list_t linked_list = {
-        anchor};
 }
