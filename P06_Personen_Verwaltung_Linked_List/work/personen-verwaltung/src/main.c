788222cafa19599a8e9fbd25f87bc138a267f9a6
Manuel <manuel@strenge.ch>
2024-04-02 18:07:17 +0000

    ahhhh

--- P06_Personen_Verwaltung_Linked_List/work/personen-verwaltung/src/main.c
+++ P06_Personen_Verwaltung_Linked_List/work/personen-verwaltung/src/main.c
@@ -17,12 +17,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
-//End: das Programm wird beendet
+// End: das Programm wird beendet
 void end();
-
-//Show: eine komplette Liste aller gespeicherten Personen wird in alphabetischer Reihenfolge ausgegeben
+// Show: eine komplette Liste aller gespeicherten Personen wird in alphabetischer Reihenfolge ausgegeben
 void show_p();
+person_t find_person();
 
 /**
  * @brief Main entry point.
@@ -30,38 +29,47 @@ void show_p();
  * @param[in] argv  The command line arguments...
  * @returns Returns EXIT_SUCCESS (=0) on success, EXIT_FAILURE (=1) there is an expression syntax error.
  */
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // BEGIN-STUDENTS-TO-ADD-CODE
     bool running = true;
 
-    while (running) {
+    while (running)
+    {
         printf("I(nsert), R(emove), S(how), C(lear), E(nd):\n");
 
         int c = getchar();
-        //clear out stdin
-        while(getchar() != '\n');
+        // clear out stdin
+        while (getchar() != '\n');
 
-        person_t person;
-        switch (c) {
-            case 'e' | 'E':
-                running = false;
-                break;
-            case 'i' | 'I':
-                person= create_person();
-                insert_p(person);
-
-                break;
-            case 'r' | 'R':
-                break;
-            case 's' | 'S':
-                show_p();
-                break;
-            case 'c' | 'C':
-                clear_p();
-                break;
-            default:
-                printf("what?");
-                break;
+        switch (c)
+        {
+        case 'e' | 'E':
+            running = false;
+            break;
+        case 'i' | 'I':
+        {
+            person_t person;
+            person = create_person();
+            insert_p(person);
+        }
+        break;
+        case 'r' | 'R':
+        {
+            person_t person;
+            person = find_person();
+            remove_p(person);
+        }
+        break;
+        case 's' | 'S':
+            show_p();
+            break;
+        case 'c' | 'C':
+            clear_p();
+            break;
+        default:
+            printf("what?");
+            break;
         }
     }
 
@@ -69,11 +77,15 @@ int main(int argc, char *argv[]) {
     return EXIT_SUCCESS;
 }
 
-
-void show_p() {
-
+void show_p()
+{
 }
 
-void end() {
+person_t find_person()
+{
+    return ;
+}
 
+void end()
+{
 }
