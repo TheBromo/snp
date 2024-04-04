8bf4147a88e0a11ae251d42ff26c5747aedaef62
TheBromo <manuel@strenge.ch>
2024-04-04 18:20:59 +0200

    tempsave

--- P06_Personen_Verwaltung_Linked_List/work/personen-verwaltung/src/list.c
+++ P06_Personen_Verwaltung_Linked_List/work/personen-verwaltung/src/list.c
@@ -4,6 +4,7 @@
 
 #include "list.h"
 #include <stdlib.h>
+#include "person.h"
 
 node_t headElement;
 node_t * const anchor = &headElement;
@@ -11,24 +12,47 @@ node_t * const anchor = &headElement;
 // Remove: der Benutzer wird aufgefordert, die Daten einer zu löschenden Person einzugeben
 void remove_p(person_t person)
 {
+    //TODO check when end is reached
+    node_t *current = anchor->next;
+    node_t *previous;
+    while (person_compare(&current->content, &current->next->content) != 0 || current->next == NULL)
+    {
+        previous = current;
+        current = current->next;
+    }
+
+    previous->next = current->next;
+    free(current);
 }
+
 
 // Clear: alle Personen werden gelöscht
 void clear_p()
 { 
    node_t *current = anchor->next; 
-   while (current != anchor)
+   while (current->next != NULL)
    {
         node_t *temp = current;
         current = current->next;
         free(temp);
    }
-   anchor->next = anchor;
+   anchor->next = NULL;
 }
 
 // Insert: der Benutzer wird aufgefordert, eine Person einzugeben
 void insert_p(person_t person)
 {
+    //check when end is reached
+    node_t *current = anchor->next;
+    node_t *previous;
+    while (person_compare(&current->content, &current->next->content) > 0 || current->next == NULL)
+    {
+        previous = current;
+        current = current->next;
+    }
+     
+
+
 }
 
 node_t *nalloc()
--- P06_Personen_Verwaltung_Linked_List/work/personen-verwaltung/src/person.c
+++ P06_Personen_Verwaltung_Linked_List/work/personen-verwaltung/src/person.c
@@ -3,14 +3,10 @@
 //
 #include "person.h"
 #include "stdlib.h"
-
-person_t create_person(){
-    person_t result={};
-
-    return result;
-}
+#include <string.h>
 
 
-int person_compare(const person_t *a, const person_t *b){
-
+int person_compare(const person_t *a, const person_t *b)
+{
+    return strcmp(a->name, b->name) + strcmp(a->first_name, b->first_name) + (a->age - b->age);
 }
