dcceedd15865e76b274e662c44d0ae72669dc908
Manuel <manuel@strenge.ch>
2024-05-07 20:32:06 +0000

    tempsave

--- P09_File_Operations/personen-verwaltung-persistent/src/person.c
+++ P09_File_Operations/personen-verwaltung-persistent/src/person.c
@@ -9,8 +9,10 @@ int person_compare(const person_t *a, const person_t *b)
 	assert(a);
 	assert(b);
 	int res = strncmp(a->name, b->name, NAME_LEN);
-	if (res == 0) res = strncmp(a->first_name, b->first_name, NAME_LEN);
-	if (res == 0) res = a->age - b->age;
+	if (res == 0)
+		res = strncmp(a->first_name, b->first_name, NAME_LEN);
+	if (res == 0)
+		res = a->age - b->age;
 	return res;
 }
 
@@ -18,30 +20,26 @@ int person_read(person_t *p)
 {
 	assert(p);
 	assert(NAME_LEN == 20);
-	memset(p, 0, sizeof(person_t));  
- 
+	memset(p, 0, sizeof(person_t));
+
 	return scanf("%19s %19s %d", p->name, p->first_name, &(p->age)) == 3;
 }
 
-//operations for persistency lab
-
+// operations for persistency lab
 
 static const int max_len = 128; //!!!könnte man schöner lösen, scia
 
-int person_to_csv_string(person_t* person, char* s)
+int person_to_csv_string(person_t *person, char *s)
 {
 	// BEGIN-STUDENTS-TO-ADD-CODE
-	
+	snprintf(s, sizeof(s), "%s;%s;%u", person->name, person->first_name, person->age);
+	return strlen(s);
 	// END-STUDENTS-TO-ADD-CODE
 }
 
-
-void person_from_csv_string(person_t* person, char* s)
+void person_from_csv_string(person_t *person, char *s)
 {
 	// BEGIN-STUDENTS-TO-ADD-CODE
-
+	sscanf(s,"%s;%s;%u", person->name, person->first_name, person->age);
 	// END-STUDENTS-TO-ADD-CODE
 }
-
-
-
